1888b788d6d9eeb9bd85615a838fbed10a0cdb04
kbutz <kylebutz@gmail.com>
2018-05-13 07:47:21 -0500

    Added convenience endpoints for viewing and clearing in memory cache

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -22,7 +23,7 @@ var upgrader = websocket.Upgrader{
 // Define our message object
 type Message struct {
 	Name    string `json:"name"`
-	Message  string `json:"message"`
+	Message string `json:"message"`
 }
 
 func main() {
@@ -33,11 +34,17 @@ func main() {
 	// Configure websocket route
 	http.HandleFunc("/ws", handleConnections)
 
+	// Convenience method for clearing in-memory message cache if things get out of control
+	http.HandleFunc("/clear-messages", clearMessages)
+
+	// Convenience method to view all messages for testing
+	http.HandleFunc("/view-all", viewAllMessages)
+
 	// Start go routine listening for incoming chat messages
 	go handleMessages()
 
 	// Start the server on localhost port 8000 and log any errors
-	log.Println("http server started on :8000")
+	log.Println("HTTP server started on :8000")
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -55,23 +62,21 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 
 	// Register our new client
 	clients[ws] = true
-	log.Printf("new client registered... ")
+	log.Printf("New client registered")
 
-  // populate message history for new connection
+	// populate message history for new connection
 	loadMessages(ws)
 
-  // enters loop until??
 	for {
 		var msg Message
 		// Read in a new message as JSON and map it to a Message object
 		err := ws.ReadJSON(&msg)
 		if err != nil {
-			log.Printf("error reading json: %v", err)
+			log.Printf("Error reading json: %v", err)
 			delete(clients, ws)
 			break
 		}
 		// Send the newly received message to the broadcast channel
-		log.Printf("handleConnections again...")
 		broadcast <- msg
 
 		// add message to broadcast slice
@@ -85,23 +90,22 @@ func handleConnections(w http.ResponseWriter, r *http.Request) {
 func loadMessages(ws *websocket.Conn) {
 	if len(allMessages) > 10 {
 		log.Printf(string(len(allMessages)))
-		for _, messageHistory := range allMessages[len(allMessages) - 10 : len(allMessages)] {
-			err := ws.WriteJSON(messageHistory)
-			if err != nil {
-				log.Printf("error writing json: %v", err)
-				ws.Close()
-				delete(clients, ws)
-			}
+		for _, messageHistory := range allMessages[len(allMessages)-10 : len(allMessages)] {
+			sendMessage(messageHistory, ws)
 		}
 	} else {
 		for _, messageHistory := range allMessages {
-			err := ws.WriteJSON(messageHistory)
-			if err != nil {
-				log.Printf("error writing json: %v", err)
-				ws.Close()
-				delete(clients, ws)
-			}
+			sendMessage(messageHistory, ws)
 		}
+	}
+}
+
+func sendMessage(messageHistory Message, ws *websocket.Conn) {
+	err := ws.WriteJSON(messageHistory)
+	if err != nil {
+		log.Printf("Error writing json: %v", err)
+		ws.Close()
+		delete(clients, ws)
 	}
 }
 
@@ -110,22 +114,29 @@ func saveMessage(message Message) {
 	allMessages = append(allMessages, message)
 }
 
+func clearMessages(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Clearing messages")
+	allMessages = allMessages[len(allMessages)-10 : len(allMessages)]
+}
+
+func viewAllMessages(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Viewing all messages")
+	json.NewEncoder(w).Encode(allMessages)
+}
+
 func handleMessages() {
 	for {
-		log.Printf("handleMessages again...")
 		// Grab the next message from the broadcast channel
-		// broadcast <- Message{"test", "test", "messageTest"}
 		msg := <-broadcast
-		// msg2 := <-broadcast
+
 		// Send it out to every client that is currently connected
 		for client := range clients {
 			err := client.WriteJSON(msg)
 			if err != nil {
-				log.Printf("error writing json: %v", err)
+				log.Printf("Error writing json: %v", err)
 				client.Close()
 				delete(clients, client)
 			}
-
 		}
 	}
 }
